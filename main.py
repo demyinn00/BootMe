e23edfe2952f6cc851328ae1e0fac8f3ea47f8c3
David Em-Yinn <demyinn@ucsd.edu>
2023-08-08 12:21:22 -0700

    Patch<main>: Removed config from repo. Main creates a default config if one dne

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from scripts.trigger_actions import (trigger_env, on_click_kill_all, on_click_clean_desktop)
 import json
+import os
 
 root = tk.Tk()
 root.title("BootMe")
@@ -10,7 +11,55 @@ root.configure(bg='#a9927d')
 title_label = tk.Label(root, text="BootMe", font=("Helvetica", 24), bg='#a9927d', fg='#5c5241')
 title_label.grid(row=0, column=0, columnspan=2, sticky="nsew")
 
-with open('config.json') as config_json:
+config_file_path = "config.json"
+
+if not os.path.exists(config_file_path):
+  default_config = {
+    "environments": [
+      {
+        "name": "Standard Enviornment",
+        "links": [
+          "https://chat.openai.com/"
+        ],
+        "apps": [],
+        "spotify_type": "album",
+        "spotify_id": "18NOKLkZETa4sWwLMIm0UZ"
+      },
+      {
+        "name": "Recruiting Environment",
+        "links": [
+          "https://www.indeed.com/jobs?l=Los+Angeles%2C+CA&mna=&=&aceid=&gclid=Cj0KCQjwoK2mBhDzARIsADGbjerBCq7yKXkuBzkVAoQAdrNS2UeV0rYpsdxzsaTcjoQ0GrQGUKzVgm4aAk3dEALw_wcB&gclsrc=aw.ds&from=mobRdr&utm_source=%2Fm%2F&utm_medium=redir&utm_campaign=dt",
+          "https://www.linkedin.com/jobs/",
+          "https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/"
+        ],
+        "apps":[],
+        "spotify_type": "album",
+        "spotify_id": "18NOKLkZETa4sWwLMIm0UZ"
+      },
+      {
+        "name": "Extra Environment 1",
+        "links": [
+          "https://www.google.com/"
+        ],
+        "apps":[],
+        "spotify_type": "none",
+        "spotify_id": "none"
+      },
+      {
+        "name": "Extra Environment 2",
+        "links": [
+          "https://www.google.com/"
+        ],
+        "apps":[],
+        "spotify_type": "playlist",
+        "spotify_id": "spotify:playlist:5sXRm52jnGFLluxfgeZ1Ng"
+      }
+    ]
+  }
+  with open(config_file_path, 'w') as output_file: 
+    json.dump(default_config, output_file)
+
+with open(config_file_path) as config_json:
   data = json.load(config_json)
   button_text = [env['name'] for env in data['environments']] + ["Kill All", "Clean Desktop"]
 
