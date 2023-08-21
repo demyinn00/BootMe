bf0f916381f4002a80f7a4639677d024d548ac08
David Em-Yinn <demyinn@ucsd.edu>
2023-08-20 21:45:04 -0700

    feat<Save settings>: Settings button has been configured to read from config.json and write to config.json

---
+++ scripts/config_manager.py
@@ -0,0 +1,71 @@
+import json
+import os
+import tkinter as tk
+
+class ConfigManager:
+  def __init__(self, config_path):
+    self.config_path = config_path
+    self.config_exists()
+
+  def config_exists(self):
+    if not os.path.exists(self.config_path):
+      self.create_default_config()
+
+  def create_default_config(self):
+    default_config = {
+      "environments": [
+        {
+          "name": "Standard Enviornment",
+          "links": [
+            "https://google.com/"
+          ],
+          "apps": [],
+          "spotify_type": "album",
+          "spotify_id": "18NOKLkZETa4sWwLMIm0UZ"
+        },
+        {
+          "name": "Recruiting Environment",
+          "links": [
+            "https://www.indeed.com/jobs?l=Los+Angeles%2C+CA&mna=&=&aceid=&gclid=Cj0KCQjwoK2mBhDzARIsADGbjerBCq7yKXkuBzkVAoQAdrNS2UeV0rYpsdxzsaTcjoQ0GrQGUKzVgm4aAk3dEALw_wcB&gclsrc=aw.ds&from=mobRdr&utm_source=%2Fm%2F&utm_medium=redir&utm_campaign=dt",
+            "https://www.linkedin.com/jobs/",
+            "https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/"
+          ],
+          "apps":[],
+          "spotify_type": "album",
+          "spotify_id": "18NOKLkZETa4sWwLMIm0UZ"
+        },
+        {
+          "name": "Extra Environment 1",
+          "links": [
+            "https://www.google.com/"
+          ],
+          "apps":[],
+          "spotify_type": "none",
+          "spotify_id": "none"
+        },
+        {
+          "name": "Extra Environment 2",
+          "links": [
+            "https://www.google.com/"
+          ],
+          "apps":[],
+          "spotify_type": "playlist",
+          "spotify_id": "spotify:playlist:5sXRm52jnGFLluxfgeZ1Ng"
+        }
+      ], 
+      "workflows": {
+        "kill_all_apps": [],
+        "clean_desktop_ignore_list": ["TRASH_ME", "ORGANIZE_ME"]
+      }
+    }
+    self.write_config(default_config)
+
+  def read_config(self):
+    if not os.path.exists(self.config_path):
+      return {}
+    with open(self.config_path, "r") as f:
+      return json.load(f)
+    
+  def write_config(self, data):
+    with open(self.config_path, "w") as f:
+      json.dump(data, f, indent=4)
--- windows/settings_window.py
+++ windows/settings_window.py
@@ -1,5 +1,12 @@
+import os
+import platform
 import tkinter as tk
 from tkinter import ttk
+from scripts.config_manager import ConfigManager
+
+# Settings Window class is deprecated and should be deleted
+# Keep for now, until finalized
+# Aug 20, 2023
 
 class SettingsWindow(tk.Toplevel):
 
@@ -9,14 +16,35 @@ class SettingsWindow(tk.Toplevel):
     self.geometry('900x400')
     self.configure(bg='#a9927d')
 
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    self.config_manager = ConfigManager(os.path.join(script_dir, "config.json"))
+    self.config_data = self.config_manager.read_config() or {}
+
     # Sidebar Frame
     self.sidebar_frame = tk.Frame(self, bg='#a9927d', width=250)
     self.sidebar_frame.grid(row=0, column=0, sticky="ns")
     self.grid_rowconfigure(0, weight=1)  # Make the row stretchable
 
     # Content Frame
-    self.content_frame = tk.Frame(self, bg='#5c5241', width=650, relief="sunken")
-    self.content_frame.grid(row=0, column=1, sticky="nsew")
+    self.content_canvas = tk.Canvas(self, bg='#5c5241')
+    self.content_canvas.grid(row=0, column=1, sticky="nsew")
+    self.content_scrollbar = tk.Scrollbar(self, command=self.content_canvas.yview)
+    self.content_scrollbar.grid(row=0, column=2, sticky='ns')
+    self.content_canvas.config(yscrollcommand=self.content_scrollbar.set)
+
+    if platform.system() == "Darwin":  # macOS
+      self.content_canvas.bind('<Button-4>', self.on_mousewheel_mac)
+      self.content_canvas.bind('<Button-5>', self.on_mousewheel_mac)
+    else:  # Windows and Linux
+      self.content_canvas.bind('<MouseWheel>', self.on_mousewheel)
+
+    self.content_canvas.config(yscrollcommand=self.content_scrollbar.set)
+
+    # Content Frame inside Canvas
+    self.content_frame = tk.Frame(self.content_canvas, bg='#5c5241', relief="sunken")
+    self.content_window = self.content_canvas.create_window((0,0), window=self.content_frame, anchor="nw")
+
+    self.content_canvas.bind("<Configure>", self.on_canvas_configure)
     self.grid_columnconfigure(1, weight=1)
 
     # Add buttons/tabs to the sidebar
@@ -27,6 +55,17 @@ class SettingsWindow(tk.Toplevel):
       btn.bind("<Button-1>", self.change_tab)
       btn.bind("<Enter>", lambda e, b=btn: b.config(bg='#5c5241', fg='#a9927d'))  # Hover in
       btn.bind("<Leave>", lambda e, b=btn: b.config(bg='#a9927d', fg='#5c5241'))  # Hover out
+
+  def on_mousewheel_mac(self, event):
+    self.content_canvas.yview_scroll(-1 if event.num == 4 else 1, "units")
+
+  def on_canvas_configure(self, event):
+    width = self.content_canvas.winfo_width() if not event else event.width
+    self.content_canvas.itemconfig(self.content_window, width=width)
+    self.content_canvas.configure(scrollregion=self.content_canvas.bbox("all"))
+
+  def on_mousewheel(self, event):
+    self.content_canvas.yview_scroll(-1*(event.delta//120), "units")
 
   def change_tab(self, event):
     for widget in self.content_frame.winfo_children():
@@ -42,7 +81,7 @@ class SettingsWindow(tk.Toplevel):
       self.default_config()
 
   def default_config(self):
-    fields = ["Name", "Links", "Play Spotify", "Spotify Link"]
+    fields = ["Name", "Links", "Apps", "Play Spotify", "Spotify Link"]
     for i, field in enumerate(fields):
       self.add_label_entry_pair(field, i)
 
@@ -63,7 +102,7 @@ class SettingsWindow(tk.Toplevel):
     if field in ['Links', 'Apps', 'Ignore']:
       entry = tk.Entry(self.content_frame)
       entry.grid(row=i, column=1, padx=10, pady=10, sticky="ew")
-      add_button = tk.Button(self.content_frame, text="Add", command=self.add_input_field)
+      add_button = tk.Button(self.content_frame, text="Add", command=lambda e=entry: self.add_input_field(e))
       add_button.grid(row=i, column=2, padx=10, pady=10)
     elif field == 'Play Spotify':
       options = ['Yes', 'No']
@@ -76,8 +115,37 @@ class SettingsWindow(tk.Toplevel):
   
     self.content_frame.grid_columnconfigure(1, weight=1)
 
-  def add_input_field(self):
-    pass  # This function will add an additional input field below the current one. You can implement this further.
+  def move_widgets_down(self, start_row):
+    for child in self.content_frame.winfo_children():
+      info = child.grid_info()
+      if int(info["row"]) > start_row:
+        child.grid(row=int(info["row"])+1, column=int(info["column"]), padx=10, pady=10)
+
+  def add_input_field(self, entry_widget):
+    current_row = entry_widget.grid_info()["row"]
+    current_col = entry_widget.grid_info()["column"]
+
+    # Locate the "Add" button associated with the passed entry widget.
+    add_button_widget = next((child for child in self.content_frame.winfo_children() 
+                              if child.grid_info().get("row") == current_row 
+                              and child.grid_info().get("column") == current_col + 1 
+                              and isinstance(child, tk.Button)), None)
+    
+    # If the button widget is found, destroy it.
+    if add_button_widget:
+      add_button_widget.destroy()
+
+    # Move all widgets below the current row down to make space for the new entry.
+    self.move_widgets_down(current_row)
+
+    # Create a new entry widget below the passed entry widget.
+    new_entry = tk.Entry(self.content_frame)
+    new_entry.grid(row=current_row+1, column=1, padx=10, pady=10, sticky="ew")
+
+    # Add an 'Add' button next to the new entry field, which can also spawn new fields.
+    add_button = tk.Button(self.content_frame, text="Add", command=lambda e=new_entry: self.add_input_field(e))
+    add_button.grid(row=current_row+1, column=2, padx=10, pady=10)
+    self.on_canvas_configure(None)
 
 if __name__ == "__main__":
   root = tk.Tk()
